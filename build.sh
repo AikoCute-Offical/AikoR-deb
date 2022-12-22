3d8d586459b97bda443b1cb2e34815726cee6d8f
Aiko <aikocute@icloud.com>
2022-12-22 23:03:16 +0700

    Update

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-dpkg -b ./AikoR-linux-64/ AikoR-0.0.1-amd64.deb
+version=`curl -s https://api.github.com/repos/AikoCute-Offical/AikoR/releases/latest | grep "tag_name" | cut -d '"' -f4`
+wget https://github.com/AikoCute-Offical/AikoR/releases/download/"${version}"/AikoR-linux-64.zip || exit 1
+# Unzip the file and remove the zip file Go to the directory /etc /aikor and overwrite all file
+unzip AikoR-linux-64.zip -d ./AikoR-linux-64/etc/aikor/ || exit 1
+rm -rf AikoR-linux-64.zip || exit 1
+
+# Build the package
+dpkg -b ./AikoR-linux-64/ AikoR-"${version}"-amd64.deb || exit 1
+
+# rename Version number from file : /AikoR-linux-64/DEBIAN/control
+sed -i "s/Version: 1.0.0/Version: ${version}/g" ./AikoR-linux-64/DEBIAN/control || exit 1
